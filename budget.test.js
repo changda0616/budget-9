3e896b653329a283d9959c84b2f3a907a5c3e5d8
ChadZ <changda0616@gmail.com>
2024-01-06 17:18:55 +0800

    feat: finish no budget

--- budget.test.js
+++ budget.test.js
@@ -1,14 +1,9 @@
 import moment from "moment";
 
 class BudgetService {
-    
-    // getTargetBudget = (start, end) => { budgets.find(item => {
-    //     return moment(item.yearMonth).isSame(start, 'month');
-    // })};
 
     query(start, end) {
         const budgets = this.getAll();
-        // const targetBudget = this.
 
         if (start > end || budgets.length === 0) {
             return 0;
@@ -18,6 +13,7 @@ class BudgetService {
             const targetBudget = budgets.find(item => {
                 return moment(item.yearMonth).isSame(start, 'month');
             });
+            if (!targetBudget) return 0;
             const daysAmount = moment(start).daysInMonth();
             return targetBudget.amount / daysAmount;
         }
@@ -26,34 +22,34 @@ class BudgetService {
             const targetBudget = budgets.find(item => {
                 return moment(item.yearMonth).isSame(start, 'month');
             });
+            if (!targetBudget) return 0;
             const daysDiff = Math.abs(moment(start).diff(end, 'days')) + 1;
             const daysAmount = moment(start).daysInMonth();
             return targetBudget.amount * daysDiff / daysAmount;
-        } else {
-            const targetBudget = budgets.filter(item => {
-                return moment(item.yearMonth).isSameOrAfter(start, 'month') && moment(item.yearMonth).isSameOrBefore(end, 'month');;
-            });
-
-            return targetBudget.reduce((sum, budget, i) => {
-                if (i === 0) {
-                    const daysAmount = moment(budget.yearMonth).daysInMonth();
-                    const daysDiff = Math.abs(moment(start).diff(moment(budget.yearMonth).endOf('month'), 'days')) + 1;
-                    
-                    return sum + (budget.amount * daysDiff / daysAmount);
-                }
-
-                if (i === targetBudget.length - 1) {
-                    const daysAmount = moment(budget.yearMonth).daysInMonth();
-                    const daysDiff = Math.abs(moment(end).diff(moment(budget.yearMonth).startOf('month'), 'days')) + 1;
-                    
-                    return sum + (budget.amount * daysDiff / daysAmount);
-                }
-
-                return sum + budget.amount;
-            }, 0);
         }
-        
-        return 0;
+
+        const targetBudget = budgets.filter(item => {
+            return moment(item.yearMonth).isSameOrAfter(start, 'month') && moment(item.yearMonth).isSameOrBefore(end, 'month');;
+        });
+
+        return targetBudget.reduce((sum, budget, i) => {
+            if (i === 0) {
+                const daysAmount = moment(budget.yearMonth).daysInMonth();
+                const daysDiff = Math.abs(moment(start).diff(moment(budget.yearMonth).endOf('month'), 'days')) + 1;
+
+                return sum + (budget.amount * daysDiff / daysAmount);
+            }
+
+            if (i === targetBudget.length - 1) {
+                const daysAmount = moment(budget.yearMonth).daysInMonth();
+                const daysDiff = Math.abs(moment(end).diff(moment(budget.yearMonth).startOf('month'), 'days')) + 1;
+
+                return sum + (budget.amount * daysDiff / daysAmount);
+            }
+
+            return sum + budget.amount;
+        }, 0);
+
     }
     getAll() { }
 }
@@ -118,7 +114,7 @@ describe('BudgetService', function () {
                 yearMonth: '202403',
                 amount: 3100
             }
-        ];
+            ];
         }
         expect(service.query(new Date(2024, 0, 22), new Date(2024, 2, 1))).toBe(600);
     })
@@ -138,11 +134,49 @@ describe('BudgetService', function () {
                 yearMonth: '202405',
                 amount: 310
             }
-        ];
+            ];
         }
         expect(service.query(new Date(2024, 1, 1), new Date(2024, 2, 1))).toBe(0);
     })
 
+    it('No db data in same month, start date is 0201, end date is 0201', () => {
+        const service = new BudgetService();
+        service.getAll = () => {
+            return [{
+                yearMonth: '202401',
+                amount: 310
+            },
+            {
+                yearMonth: '202404',
+                amount: 300
+            },
+            {
+                yearMonth: '202405',
+                amount: 310
+            }
+            ];
+        }
+        expect(service.query(new Date(2024, 1, 1), new Date(2024, 1, 1))).toBe(0);
+    })
+    it('No db data in same month, start date is 0201, end date is 0201', () => {
+        const service = new BudgetService();
+        service.getAll = () => {
+            return [{
+                yearMonth: '202401',
+                amount: 310
+            },
+            {
+                yearMonth: '202404',
+                amount: 300
+            },
+            {
+                yearMonth: '202405',
+                amount: 310
+            }
+            ];
+        }
+        expect(service.query(new Date(2024, 1, 1), new Date(2024, 1, 15))).toBe(0);
+    })
 });
 
 
